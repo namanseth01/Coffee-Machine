833c7194d1128f2e8ed1ce7d1aab99d07062f40e
Naman Seth <naman.seth_cs19@gla.ac.in>
2021-08-11 23:08:05 +0530

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
      public static void main(String[] args) {
-        
+        Scanner scannerObject = new Scanner(System.in);
+        int quantityOfWater = 400;
+        int quantityOfMilk = 540;
+        int quantityOfCoffeeBeans = 120;
+        int quantityOfDisposableCups = 9;
+        int howMuchMoney = 550;
+        String choiceOfAction;
+        level1:
         while (true) {
             System.out.println("Write action (buy, fill, take, remaining, exit):");
             choiceOfAction = scannerObject.nextLine();
